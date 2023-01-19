61b960a47037608cfbc747f22de103c8c48c0b9d
Scott <scotthenry1@gmail.com>
2023-01-19 17:07:42 -0500

    Completed lab submission

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, event) {
+    const cardMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        cardMessages[i] = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardMessages;
+  }
+
+  function countDown(i) {
+    let counter = i;
+    
+    while (counter >= 0) {
+        console.log(counter);
+        counter--;
+    }
+  }
